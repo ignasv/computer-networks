86ed9fbbd8825899288683230500a9ff1397aac5
ignas <i.velza@gmail.com>
2017-01-07 23:42:10 +0200

    Code indentation

--- echo-server-c/client.c
+++ echo-server-c/client.c
@@ -1,49 +1,49 @@
 #include <stdio.h>
 #include <winsock.h>
- 
+
 #define PORT 13301
 #define BUFFER_LENGTH 1024
 #define HOST "127.0.0.1"
 
 int main() {
-    WSADATA wsaData;
-    WSAStartup(0x0202, &wsaData);
+	WSADATA wsaData;
+	WSAStartup(0x0202, &wsaData);
 
-    int curr_socket;
-    struct sockaddr_in servaddr;
+	int curr_socket;
+	struct sockaddr_in servaddr;
 
-    char send_buffer[BUFFER_LENGTH];    
-    char receive_buffer[BUFFER_LENGTH];    
+	char send_buffer[BUFFER_LENGTH];
+	char receive_buffer[BUFFER_LENGTH];
 
-    curr_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if(curr_socket < 0) {
-        printf("Socket creation failed.\n"); 
-        exit(1);
-    }
+	curr_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if (curr_socket < 0) {
+		printf("Socket creation failed.\n");
+		exit(1);
+	}
 
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(PORT);
-    servaddr.sin_addr.s_addr = inet_addr(HOST);
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(PORT);
+	servaddr.sin_addr.s_addr = inet_addr(HOST);
 
-    if (connect(curr_socket,(struct sockaddr *)&servaddr,sizeof(servaddr))!=0){
+	if (connect(curr_socket, (struct sockaddr *)&servaddr, sizeof(servaddr)) != 0) {
 		printf("Socket connection failed.\n");
 		exit(1);
 	}
 	printf("Connected!\nEnter the message to send:\n");
 
-    fgets(send_buffer, sizeof(send_buffer), stdin);
+	fgets(send_buffer, sizeof(send_buffer), stdin);
 
-    memset(&receive_buffer, 0, BUFFER_LENGTH);
+	memset(&receive_buffer, 0, BUFFER_LENGTH);
 
-    send(curr_socket, send_buffer, strlen(send_buffer), 0);
-    printf("Sent: %s\n", send_buffer);
-    
-    recv(curr_socket, receive_buffer, BUFFER_LENGTH, 0);
-    printf("Received: %s\n", receive_buffer);
-    
-    closesocket(curr_socket);
-    printf("Disconnected.\n");
+	send(curr_socket, send_buffer, strlen(send_buffer), 0);
+	printf("Sent: %s\n", send_buffer);
 
-    return 0;
+	recv(curr_socket, receive_buffer, BUFFER_LENGTH, 0);
+	printf("Received: %s\n", receive_buffer);
+
+	closesocket(curr_socket);
+	printf("Disconnected.\n");
+
+	return 0;
 }
--- echo-server-c/server.c
+++ echo-server-c/server.c
@@ -5,69 +5,69 @@
 #define BUFFER_LENGTH 1024
 
 int main() {
-    WSADATA wsaData;
-    WSAStartup(0x0202, &wsaData);
+	WSADATA wsaData;
+	WSAStartup(0x0202, &wsaData);
 
-    char buffer[BUFFER_LENGTH];
+	char buffer[BUFFER_LENGTH];
 
-    struct sockaddr_in server_address;
-    struct sockaddr_in client_address;
+	struct sockaddr_in server_address;
+	struct sockaddr_in client_address;
 
-    int listening_socket;
-    int connected_socket;
-    
-    int received_len;
-    int sent_len;
+	int listening_socket;
+	int connected_socket;
 
-    listening_socket = socket(
-        AF_INET,        //IPv4
-        SOCK_STREAM,    //two-way socket
-        IPPROTO_TCP     //TCP
-    );
-    
-    if(listening_socket < 0) {
-        printf("Socket creation failed.\n");
-        exit(1);
-    }
+	int received_len;
+	int sent_len;
 
-    memset(&server_address,0, sizeof(server_address)); 
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons (PORT);
-    server_address.sin_addr.s_addr = INADDR_ANY; //any interface
+	listening_socket = socket(
+		AF_INET,        //IPv4
+		SOCK_STREAM,    //two-way socket
+		IPPROTO_TCP     //TCP
+	);
 
-    if(bind(listening_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
-        printf("Binding socket failed.\n");
-        exit(1);
-    }
-
-    if(listen(listening_socket, 5) < 0) {    
-        printf("Listening of socket failed.\n");
+	if (listening_socket < 0) {
+		printf("Socket creation failed.\n");
 		exit(1);
-    }
-    
-    printf("Listening on %u...\n", PORT);
-    
-    while(1) {
-        memset(&buffer, 0, sizeof(buffer));
-        memset(&client_address, 0, sizeof(client_address));
+	}
 
-        size_t client_address_len = sizeof(client_address);
-        connected_socket = accept(listening_socket, (struct sockaddr *) &client_address, &client_address_len);
-        
-        if(connected_socket < 0) {
-            printf("Socket connection failed.\n");
-            exit(1);
-        }
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr = INADDR_ANY; //any interface
 
-        printf("Connection established.\n");
+	if (bind(listening_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+		printf("Binding socket failed.\n");
+		exit(1);
+	}
 
-        received_len = recv(connected_socket, buffer, sizeof(buffer), 0);
-        sent_len = send(connected_socket, buffer, received_len, 0);
+	if (listen(listening_socket, 5) < 0) {
+		printf("Listening of socket failed.\n");
+		exit(1);
+	}
 
-        printf("Client: %s. Received: %d. Sent: %d.\n", inet_ntoa(client_address.sin_addr), received_len, sent_len);
+	printf("Listening on %u...\n", PORT);
 
-        closesocket(connected_socket);
-        printf("Client disconnected.\n");
-    }
-    return 0;
+	while (1) {
+		memset(&buffer, 0, sizeof(buffer));
+		memset(&client_address, 0, sizeof(client_address));
+
+		size_t client_address_len = sizeof(client_address);
+		connected_socket = accept(listening_socket, (struct sockaddr *) &client_address, &client_address_len);
+
+		if (connected_socket < 0) {
+			printf("Socket connection failed.\n");
+			exit(1);
+		}
+
+		printf("Connection established.\n");
+
+		received_len = recv(connected_socket, buffer, sizeof(buffer), 0);
+		sent_len = send(connected_socket, buffer, received_len, 0);
+
+		printf("Client: %s. Received: %d. Sent: %d.\n", inet_ntoa(client_address.sin_addr), received_len, sent_len);
+
+		closesocket(connected_socket);
+		printf("Client disconnected.\n");
+	}
+	return 0;
 }
